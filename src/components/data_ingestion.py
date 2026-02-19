b59d3758490551729f7ba8c988ae030d20f0c555
Sai Kiran Pinnu <pinnusaikiran118@gmail.com>
2026-02-20 01:22:05 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,13 +7,15 @@ from src.exception import CustomException
 from src.logger import logging
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+
 @dataclass
-class DataIngestionConfig():
+class DataIngestionConfig:
     train_data_path:str=os.path.join("artifacts","train.csv")
     test_data_path:str=os.path.join("artifacts","test.csv")
     raw_data_path:str=os.path.join("artifacts","data.csv")
 
-class DataIngestion():
+class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
@@ -47,4 +49,7 @@ class DataIngestion():
         
 if __name__=='__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
